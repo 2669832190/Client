1e7e0b835a60ad00188722f48c220913b803a8ea
2669832190 <96279957+2669832190@users.noreply.github.com>
2024-11-18 19:31:39 +0800

    1

--- 客户端/Class/Vision.cs
+++ 客户端/Class/Vision.cs
@@ -9,14 +9,19 @@ using System.Threading.Tasks;
 
 namespace 九点标定
 {
-    internal class Vision
+    public class Vision
     {
         //标定的TB
         public CogToolBlock NPointTB;
         //相机
         public CogAcqFifoTool FifoTool;
 
-        private string NPointTB_path = Directory.GetCurrentDirectory() + "\\vpp\\NPoint.vpp";
+		/// <summary>
+		/// 下相机NPointVPP
+		/// </summary>
+		public CogToolBlock DownCameraNpointTB { get; set; }
+
+		private string NPointTB_path = Directory.GetCurrentDirectory() + "\\vpp\\NPoint.vpp";
         private string FifoTool_path = Directory.GetCurrentDirectory() + "\\vpp\\CamFifo.vpp";
 
         public void Load()
@@ -24,5 +29,22 @@ namespace 九点标定
             NPointTB = CogSerializer.LoadObjectFromFile(NPointTB_path) as CogToolBlock;
             FifoTool = CogSerializer.LoadObjectFromFile(FifoTool_path) as CogAcqFifoTool;
         }
-    }
+		/// <summary>
+		/// 保存检测Vpp
+		/// </summary>
+		/// <returns></returns>
+		public bool SaveTB() {
+			try {
+				//CogSerializer.SaveObjectToFile(InsepectionTB, _inspectTBPath);
+				//CogSerializer.SaveObjectToFile(DownCameraTB , _downTBPath);
+				//CogSerializer.SaveObjectToFile(RecheckTB , _recheckTBPath);
+				//CogSerializer.SaveObjectToFile(RecheckTB , _recheckTBPath);
+				return true;
+
+			} catch ( Exception ) {
+
+				return false;
+			}
+		}
+	}
 }
---
+++ 客户端/Forms/FrmNPointTool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cognex.VisionPro;
+using System.Windows.Forms;
+using 九点标定;
+
+
+namespace 客户端
+{
+    
+    public partial class FrmNPointTool : Form
+    {
+        public Vision frmVision;
+
+		public delegate void LoadVppDelagate();
+        public event LoadVppDelagate LoadVppEvent;
+        public FrmNPointTool()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmNPointTool_Load(object sender, EventArgs e)
+        {
+            cogToolBlockEditV21.Subject = frmVision.DownCameraNpointTB;
+        }
+
+        private void tsbSave_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("确认保存设置", "保存设置", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+            {
+                frmVision.DownCameraNpointTB = cogToolBlockEditV21.Subject;
+                frmVision.SaveTB();
+                MessageBox.Show("保存完成");
+            }
+        }
+
+        private void FrmNPointTool_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LoadVppEvent?.Invoke();
+        }
+    }
+}
